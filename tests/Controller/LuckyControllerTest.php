f43c2dea502289e497e5482417b1d7ef93963b6d
Cesar Gutierrez <a20062161@pucp.edu.pe>
2021-02-13 15:55:24 -0500

    Add unit test for controllers

---
+++ tests/Controller/LuckyControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PostControllerTest extends WebTestCase
+{
+    public function testShowPost()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/lucky/number/20');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
